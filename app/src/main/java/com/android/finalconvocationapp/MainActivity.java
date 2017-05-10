73d7b30e2564a3648e5278d7e977db2161dafc3c
Prabhakarmani <manishprabhakar@outlook.com>
2017-05-10 13:38:42 +0530

    Initial commit

---
+++ app/src/main/java/com/android/finalconvocationapp/MainActivity.java
@@ -0,0 +1,279 @@
+package com.android.finalconvocationapp;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemSelectedListener
+{
+    private TextView txtTimerDay, txtTimerHour, txtTimerMinute, txtTimerSecond;
+    private TextView tvEvent;
+    private Handler handler;
+    private Runnable runnable;
+    static TextView mDotsText[];
+    private int mDotsCount;
+    private LinearLayout mDotsLayout;
+    private RecyclerView mBlogList;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Gallery gallery = (Gallery) findViewById(R.id.gallery);
+        txtTimerDay = (TextView) findViewById(R.id.txtTimerDay);
+        txtTimerHour = (TextView) findViewById(R.id.txtTimerHour);
+        txtTimerMinute = (TextView) findViewById(R.id.txtTimerMinute);
+        txtTimerSecond = (TextView) findViewById(R.id.txtTimerSecond);
+        tvEvent = (TextView) findViewById(R.id.tvhappyevent);
+        countDownStart();
+        gallery.setAdapter(new ImageAdapter(this));
+        gallery.setOnItemSelectedListener(this);
+
+        mDotsLayout = (LinearLayout) findViewById(R.id.image_count);
+        // here we count the number of images we have to know how many dots we
+        // need
+        mDotsCount = gallery.getAdapter().getCount();
+
+        // here we create the dots
+        // as you can see the dots are nothing but "." of large size
+        mDotsText = new TextView[mDotsCount];
+
+        // here we set the dots
+        for (int i = 0; i < mDotsCount; i++)
+        {
+            mDotsText[i] = new TextView(this);
+            mDotsText[i].setText(".");
+            mDotsText[i].setTextSize(45);
+            mDotsText[i].setTypeface(null, Typeface.BOLD);
+            mDotsText[i].setTextColor(android.graphics.Color.GRAY);
+            mDotsLayout.addView(mDotsText[i]);
+        }
+        //Recycler View
+        mBlogList = (RecyclerView)findViewById(R.id.blog_list);
+        mBlogList.setHasFixedSize(true);
+        mBlogList.setLayoutManager(new LinearLayoutManager(this));
+
+        // Send a Query to the database
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Data");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    public void countDownStart() {
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(this, 1000);
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat(
+                            "yyyy-MM-dd");
+// Please here set your event date//YYYY-MM-DD
+                    Date futureDate = dateFormat.parse("2017-05-22");
+                    Date currentDate = new Date();
+                    if (!currentDate.after(futureDate)) {
+                        long diff = futureDate.getTime()
+                                - currentDate.getTime();
+                        long days = diff / (24 * 60 * 60 * 1000);
+                        diff -= days * (24 * 60 * 60 * 1000);
+                        long hours = diff / (60 * 60 * 1000);
+                        diff -= hours * (60 * 60 * 1000);
+                        long minutes = diff / (60 * 1000);
+                        diff -= minutes * (60 * 1000);
+                        long seconds = diff / 1000;
+                        txtTimerDay.setText("" + String.format("%02d", days));
+                        txtTimerHour.setText("" + String.format("%02d", hours));
+                        txtTimerMinute.setText(""
+                                + String.format("%02d", minutes));
+                        txtTimerSecond.setText(""
+                                + String.format("%02d", seconds));
+                    } else {
+                        tvEvent.setVisibility(View.VISIBLE);
+                        tvEvent.setText("The event started!");
+                        textViewGone();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        handler.postDelayed(runnable, 1 * 1000);
+    }
+    public void textViewGone() {
+        findViewById(R.id.LinearLayout10).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout11).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout12).setVisibility(View.GONE);
+        findViewById(R.id.LinearLayout13).setVisibility(View.GONE);
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<ModelClass, BlogViewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<ModelClass, BlogViewHolder>(
+                        ModelClass.class,
+                        R.layout.design_row,
+                        BlogViewHolder.class,
+                        myRef)  {
+
+
+
+                    @Override
+                    protected void populateViewHolder(BlogViewHolder viewHolder, ModelClass model,int position) {
+                        viewHolder.setTitle(model.getTitle());
+                        viewHolder.setImage(getApplicationContext(), model.getImage());
+                    }
+                };
+        mBlogList.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView arg0, View arg1, int arg2,
+                               long arg3) {
+        // TODO Auto-generated method stub
+        for (int i = 0; i < mDotsCount; i++) {
+            MainActivity.mDotsText[i].setTextColor(Color.GRAY);
+        }
+
+        MainActivity.mDotsText[arg2].setTextColor(Color.GREEN);
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+    //View Holder For Recycler View
+    public static class BlogViewHolder extends RecyclerView.ViewHolder  {
+        View mView;
+        public BlogViewHolder(View itemView) {
+            super(itemView);
+            mView= itemView;
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("http://www.google.com"));
+                    Intent browserChooserIntent = Intent.createChooser(browserIntent , "Choose browser of your choice");
+                    v.getContext().startActivity(browserChooserIntent);
+                }
+            });
+            /******** For More Android Tutorials .. Download "Master Android" Application From Play Store Free********/
+        }
+        public void setTitle(String title){
+            TextView post_title = (TextView)mView.findViewById(R.id.titleText);
+            post_title.setText(title);
+        }
+        public void setImage(Context ctx , String image){
+            ImageView post_image = (ImageView)mView.findViewById(R.id.imageViewy);
+            // We Need TO pass Context
+            Picasso.with(ctx).load(image).into(post_image);
+        }    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera)
+        {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery)
+        {
+
+        } else if (id == R.id.nav_slideshow)
+        {
+
+        } else if (id == R.id.nav_manage)
+        {
+
+        } else if (id == R.id.nav_share)
+        {
+
+        } else if (id == R.id.nav_send)
+        {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
